8504a4f9328ddee5e24b1003f3865953c4ce9ef3
Carolina Pereira <carolina.pereira@estudiantes.utec.edu.uy>
2022-08-06 14:03:00 -0300

    funciona

--- ecommerce_project/products/urls.py
+++ ecommerce_project/products/urls.py
@@ -3,5 +3,5 @@ from products.views import products_list, create_product
 
 urlpatterns = [
     path('products_list/', products_list, name='products_list'),
-    path('create-product/', create_product, name='create_product'),
+    path('create_product/', create_product, name='create_product'),
 ]
