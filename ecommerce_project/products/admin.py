b117a655ee5bbe41290900543495f8c91ce512b4
Carolina Pereira <carolina.pereira@estudiantes.utec.edu.uy>
2022-08-25 18:38:03 -0300

    agrego al panel administrador el crud de productos

--- ecommerce_project/products/admin.py
+++ ecommerce_project/products/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from products.models import Products
 
 # Register your models here.
-admin.site.register(Products)
+@admin.register(Products)
+class Products_Admin(admin.ModelAdmin):
+    list_display: ['name', 'price', 'description', 'stock', 'size']
