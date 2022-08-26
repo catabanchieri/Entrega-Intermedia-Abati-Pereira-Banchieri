58394364e4f50602e5d771f838e3aa5e4d6de222
Carolina Pereira <carolina.pereira@estudiantes.utec.edu.uy>
2022-08-25 22:31:37 -0300

    creo html para blog

--- ecommerce_project/ecommerce/urls.py
+++ ecommerce_project/ecommerce/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('',home, name='home'), # home page
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')), #se utiliza el include para que se pueda acceder a las urls de products
+    path('blog/', include('blog.urls')), 
     path('business/', include('business.urls')),    
     path('business/',business, name='business')
 ]
