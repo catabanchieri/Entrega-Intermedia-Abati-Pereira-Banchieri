24d1e2c8eb92a9e724140ed163cd8773189d0061
Carolina Pereira <carolina.pereira@estudiantes.utec.edu.uy>
2022-08-28 23:21:27 -0300

    Funciona el crud

--- ecommerce_project/products/forms.py
+++ ecommerce_project/products/forms.py
@@ -1,17 +1,38 @@
-from pyexpat import model
+from random import choices
+from tkinter import Widget
 from django import forms
+from django.forms import ModelForm
 
-class Forms_products(forms.Form):
+from .models import Products
+
+
+class Forms_products(forms.ModelForm):
+    choices = [('S', 'S'), ('M', 'M'), ('L', 'L'),
+               ('XL', 'XL'), ('XXL', 'XXL')]
+    size = forms.ChoiceField(choices=choices)
+
+    class Meta:
+        model = Products
+        fields = ['name', 'price', 'description', 'stock', 'size']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control'}),
+            'size': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+
+
+'''
     name = forms.CharField(max_length=50)
     price = forms.DecimalField(max_digits=10, decimal_places=2)
     description = forms.CharField(max_length=200)
     stock = forms.IntegerField()
-    size = forms.CharField(max_length=5)  # S, M, L, XL, XXL
-    #available = forms.BooleanField(default=True)
-    
-    
+    size = forms.CharField(max_length=5)
+    #available = forms.BooleanField(default=True)    
     #image = forms.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-
+'''
 
 
 class Forms_category(forms.Form):
