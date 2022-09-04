dc363f0cdf9e995882ecfb0cf43b7c1ad4829f88
Catalina Banchieri <cbanchieri.smc@gmail.com>
2022-09-04 00:58:19 -0300

    crear perfil ok

--- ecommerce_project/users/urls.py
+++ ecommerce_project/users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 path('login/',login_request,name='login'),
 path('register/',register,name='register'),
 path('logout/', LogoutView.as_view(template_name='users/logout.html'),name='logout'),
-path('new-profile/',create_profile,name='new-profile')
+path('create_profile/',create_profile,name='create-profile')
 
 ]
--- ecommerce_project/users/views.py
+++ ecommerce_project/users/views.py
@@ -1,11 +1,12 @@
 
-from collections import UserList
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout, authenticate
 from users.forms import User_registration_form, Profile_form
 from django.contrib.auth.views import LogoutView
 from users.models import Profile
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -35,10 +36,12 @@ def register(request):
 
             return redirect('login')
         
-        context={'errors':form.errors}
+        
         form= User_registration_form
-        context['form']=form
-        return render(request, 'users/register.html',{'form':form})
+        
+        return render(request,'users/register.html',{'error':'Datos incorrectas','form':form})
+       
+        
 
     elif request.method=='GET':
         form=User_registration_form()
@@ -46,16 +49,25 @@ def register(request):
 
 
 # ## PERFILES
+
+@login_required
 def create_profile(request):
     if request.method=='POST':
+
          form=Profile_form(request.POST)
          if form.is_valid():
-             form.save()
-             return render (request, 'home/index.html',context=context)  
+
+            u=User.objects.get(username=request.user)
+            profile=Profile.objects.create(user=u,name=form.cleaned_data['name'],surname=form.cleaned_data['surname'],birth_date=form.cleaned_data['birth_date'],address=form.cleaned_data['address'])
+
+            
+            return render (request, 'home/index.html',context={})  
+        
+         return render(request,'users/create_profile.html',{'error': 'Datos incorrectos','form':form})
         
 
     elif request.method == 'GET':
-         form = Profile_form()
-         context = {'form':form}
-         return render(request, 'users/new_profile.html', context=context)
+        form = Profile_form()
+        context = {'form':form}
+        return render(request, 'users/create_profile.html', context=context)
      
